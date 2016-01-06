9528153d922e9f9262d322ebe1860721c4151195
annetts <Annett.Saarik@hotmail.com>
2016-01-06 12:38:43 +0200

    Merge

--- Algorithmics/src/main/java/com/project/data/algorithms/MiniMax.java
+++ Algorithmics/src/main/java/com/project/data/algorithms/MiniMax.java
@@ -1,68 +1,109 @@
 package com.project.data.algorithms;
+
 import com.project.data.GameBoardHelper;
 
 import java.lang.Math;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class MiniMax extends AbstractAlgorithm {
 	private LinkedList<MiniMax> children = new LinkedList<MiniMax>();
 	private double value = 0;
 	private boolean isCapture;
+	private int depth = 3;
+	private static Map<Byte[], Integer> boardEvalueteMap = new HashMap<>();
+	private int recursion = 1;
 
 	public MiniMax() {
 		super("Random2 Algorithm");
 	}
 
+	public static int evaluate(Byte[] beforeBoard) {
+		int score = 0;
+		for (int i = 1; i < 65; i++) {
+			if (beforeBoard[i] != 69)
+				score = score + beforeBoard[i];
+		}
+		return score;
+	}
+
+	// Returns the board with the smallest score.
+	public static Byte[] minBoard(Byte[] a, Byte[] b) {
+		if (evaluate(a) <= evaluate(b))
+			return a;
+		else
+			return b;
+	}
+
+	// Returns the board with the largest score.
+	public static Byte[] maxBoard(Byte[] a, Byte[] b) {
+		if (evaluate(a) >= evaluate(b))
+			return a;
+		else
+			return b;
+	}
+
+	public static Integer opponent(Integer player) {
+		if (player == 1)
+			return 2;
+		else
+			return 1;
+	}
+
+	public Byte[] findBestBoard(int color, Byte[] boardEvalueteMap) {
+		for (Byte bestBoard : boardEvalueteMap) {
+			if (color == 1)
+				bestBoard = maxBoard(bestBoard, iterator.next());
+			else
+				bestBoard = minBoard(bestBoard, iterator.next());
+		}
+		return bestBoard;
+	}
+
+	// The minimax algorithm without alpha-beta cutoff.
 	@Override
 	public Byte[] nextBoard(Byte[] beforeBoard, Integer color) {
 
-		List<Byte[]> possibleMove = GameBoardHelper.generateMoves(beforeBoard, color);
+		List<Byte[]> possibleMoves = GameBoardHelper.generateMoves(beforeBoard, color);
+		
+		if (possibleMoves.size() == 0)
+			return null;
+		
+		if (color == 1){
+			for (Byte[] board : possibleMoves) {
+				boardEvalueteMap.put(board, evaluate(board));
+				return findBestBoard(color, boardEvalueteMap);
+			}
+		}
+		if (color == 2){
+			
+		}
+				
 		
 		return null;
 	}
 
-	
-
-	/*public double minimax(boolean isComputer) {
-		if (this.getChildren().isEmpty()) {
-			return this.getValue();
+	public static Byte[] minimax(Byte[] beforeBoard, int recursion, Integer color){
+			if (recursion > 0) {
+				 List<Byte[]> possibleMoves = null;
+		         possibleMoves = GameBoardHelper.generateMoves(beforeBoard, color);
+		         if (possibleMoves.size() == 0)
+		            return null;
+		         if (color == 1) {   // White aka 1 - min node.
+		        	 for (Byte[] board : possibleMoves) {
+		        		 boardEvalueteMap.put(minimax(executeMove(board, beforeBoard), 
+		                                     recursion - 1, opponent(color)));
+		            return findBestBoard(color, boardEvalueteMap);
+		         } else {      // Black aka 2 - max node.
+		        	 for (Byte[] board : possibleMoves) {
+		        		 boardEvalueteMap.add(minimax(executeMove(board, beforeBoard), 
+		                                           recursion - 1, opponent(color)));
+		            return findBestBoard(color, boardEvalueteMap);
+		         } else {
+		         return board;   // Recursion done -> leaf in game tree.
+		   }
+			
 		}
-		if (isComputer) {
-			double a = Double.MAX_VALUE;
-			for (Iterator<MiniMax> i = children.iterator(); i.hasNext();) {
-				MiniMax minimax = (MiniMax) i.next();
-				a = Math.min(a, minimax.minimax(!isComputer));
-			}
-			return a;
-		} else {
-			double a = Double.MAX_VALUE;
-			for (Iterator<MiniMax> i = children.iterator(); i.hasNext();) {
-				MiniMax minimax = (MiniMax) i.next();
-				a = Math.min(a, minimax(!isComputer));
-			}
-			return a;
-		}
-	}
-
-	public MiniMax getMove(boolean isComputer) {
-		if (children.isEmpty()) {
-			return null;
-		}
-
-		MiniMax best = null;
-		double maxScore = (isComputer ? Double.MIN_EXPONENT : Double.MAX_VALUE);
-		for (Iterator<MiniMax> i = children.iterator(); i.hasNext();) {
-			MiniMax child = (MiniMax) i.next();
-			double value = child.minimax(isComputer);
-			if (best == null || value * (isComputer ? 1 : -1) > maxScore * (isComputer ? 1 : -1)) {
-				maxScore = value;
-				best = child;
-			}
-		}
-
-		return best;
-	}*/
-}
-
